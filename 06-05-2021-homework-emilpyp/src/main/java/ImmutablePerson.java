263ab45c0c2b639cd4b68cebe9eeeea752d9a36b
emilpyp <83601209+emilpyp@users.noreply.github.com>
2021-05-07 12:38:18 +0400

    Update ImmutablePerson.java

--- 06-05-2021-homework-emilpyp/src/main/java/ImmutablePerson.java
+++ 06-05-2021-homework-emilpyp/src/main/java/ImmutablePerson.java
@@ -3,7 +3,7 @@ public class ImmutablePerson {
     private final String lastname;
     private final int age;
 
-    ImmutablePerson(String name, String lastname, int age){
+    public ImmutablePerson(String name, String lastname, int age){
         this.name = name;
         this.lastname = lastname;
         this.age = age;
@@ -19,18 +19,6 @@ public class ImmutablePerson {
         return this.age;
     }
 
-    public ImmutablePerson setName(String newName){
-        return new ImmutablePerson(newName, this.lastname, this.age);
-    }
-
-    public ImmutablePerson setLastname(String newLastname){
-        return new ImmutablePerson(this.name, newLastname, this.age);
-    }
-
-    public ImmutablePerson setAge(int newAge){
-        return new ImmutablePerson(this.name, this.lastname, newAge);
-    }
-
     @Override
     public String toString(){
         return "Name: " + this.name +
@@ -40,14 +28,8 @@ public class ImmutablePerson {
 
     public static void main(String[] args) {
         ImmutablePerson taylor = new ImmutablePerson("Taylor", "Swift", 30);
-        ImmutablePerson twinTaylor = taylor.setAge(30);
-
-        //System.out.println(emil);
+        //taylor.age = 5;
 
         System.out.println(taylor);
-        System.out.println();
-        System.out.println(twinTaylor);
-        System.out.println();
-        System.out.printf("%s == %s: %b", taylor.getName(), twinTaylor.getName(), (taylor == twinTaylor));
     }
 }
